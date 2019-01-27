7b89800b076ac836c8eb5e9708681de16430b207
PROkOLI <pinter.oliver@gmail.com>
2019-01-27 23:27:05 +0100

    Update Marketplace.sol

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.0;
 
 import "node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./CircuitBreaker";
 
 /** @title Marketplace */
 contract Marketplace is Ownable {  
